eadf24e5de5885d36eefac32c07077b92badb8d9
SnowCharm <619022098@qq.com>
2022-10-12 18:51:26 +0800

    initial commit

---
+++ src/test/java/sustech/ooad/a2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sustech.ooad.a2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
