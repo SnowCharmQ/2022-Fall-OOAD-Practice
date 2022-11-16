a3366eebc6ff7858495600388b10c8af6d0ccdcf
SnowCharm <619022098@qq.com>
2022-11-16 19:34:56 +0800

    initialize the original codes for ass4

---
+++ Assignment4/src/Origin/ITStaff.java
@@ -0,0 +1,13 @@
+package Origin;
+
+public interface ITStaff {
+    public String working();
+
+    public int getSalary();
+
+    public String getName();
+
+    public String getUserID();
+
+    public String toString();
+}
