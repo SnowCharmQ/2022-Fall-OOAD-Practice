1ef5285d52d86c53f3a3bb6e84ded8aa99f25d25
SnowCharm <619022098@qq.com>
2023-01-31 11:41:00 +0800

    implement Adapter.java

--- AdapterPattern/src/adapter/Adapter.java
+++ AdapterPattern/src/adapter/Adapter.java
@@ -1,5 +1,9 @@
 package adapter;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
 
 public class Adapter implements FileOperateInterfaceV2 {
@@ -11,21 +15,50 @@ public class Adapter implements FileOperateInterfaceV2 {
 
     @Override
     public List<StaffModel> readAllStaff() {
-        return null;
+        return adaptee.readStaffFile();
     }
 
     @Override
     public void listAllStaff(List<StaffModel> list) {
-
+        adaptee.printStaffFile(list);
     }
 
     @Override
     public void writeByName(List<StaffModel> list) {
-
+        list.sort(Comparator.comparing(StaffModel::getName));
+        this.writeByFilename(list, "staffByName");
     }
 
     @Override
     public void writeByRoom(List<StaffModel> list) {
+        list.sort(Comparator.comparing(StaffModel::getRoom));
+        this.writeByFilename(list, "staffByRoom");
+    }
 
+    public void writeByFilename(List<StaffModel> list, String ss) {
+        try {
+            if (list.size() == 0) {
+                System.out.println("No information to be written");
+                return;
+            }
+            String filename = String.format("AdapterPattern/src/adapter/%s.txt", ss);
+            FileWriter f = new FileWriter(filename);
+            BufferedWriter bufw = new BufferedWriter(f);
+            String str;
+            for (StaffModel s : list) {
+                str = s.getName() + ":" +
+                        s.getTitle() + "," +
+                        s.getRoom() + "," +
+                        s.getEmail() + "," +
+                        s.getLink();
+                bufw.write(str);
+                bufw.newLine();
+            }
+            bufw.flush();
+            bufw.close();
+            System.out.println("finish writing");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
